8516e2266bdcedeb202f33109706417c6b8d57c7
Stefan Hyltoft <stefanhyltoft@Stefans-iMac.local>
2017-09-25 19:55:04 +0200

    Done with create song mutation

--- client/index.js
+++ client/index.js
@@ -5,6 +5,7 @@ import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import App from './components/App';
 import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
 
 const client = new ApolloClient({});
 
@@ -15,6 +16,7 @@ const Root = () => {
         <Route path="/" component={App}>
           <IndexRoute component={SongList} />
         </Route>
+        <Route path="song/new" component={SongCreate} />
       </Router>
     </ApolloProvider>
   );
