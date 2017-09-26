673bae2331fa4fb2cf46de56607e59352d6419ec
Stefan Hyltoft <stefanhyltoft@Stefans-iMac.local>
2017-09-26 21:58:30 +0200

    More woooork

--- client/index.js
+++ client/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory, IndexRoute } from 'react-router';
-import ApolloClient from 'apollo-client';
-import { ApolloProvider } from 'react-apollo';
-import App from './components/App';
-import SongList from './components/SongList';
-import SongCreate from './components/SongCreate';
+import "./style/style.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Route, hashHistory, IndexRoute } from "react-router";
+import ApolloClient from "apollo-client";
+import { ApolloProvider } from "react-apollo";
+import App from "./components/App";
+import SongList from "./components/SongList";
+import SongCreate from "./components/SongCreate";
+import SongDetail from "./components/SongDetail";
 
 const client = new ApolloClient({});
 
@@ -17,12 +19,10 @@ const Root = () => {
           <IndexRoute component={SongList} />
         </Route>
         <Route path="songs/new" component={SongCreate} />
+        <Route path="songs/:id" component={SongDetail} />
       </Router>
     </ApolloProvider>
   );
 };
 
-ReactDOM.render(
-  <Root />,
-  document.querySelector('#root')
-);
+ReactDOM.render(<Root />, document.querySelector("#root"));
